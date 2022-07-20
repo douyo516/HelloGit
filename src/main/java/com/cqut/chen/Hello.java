0dce866e684ca1d0eb62c12d3d7ea22aeabc5013
Ying <1553444232@qq.com>
2022-07-20 18:41:35 +0800

    克隆添加

--- src/main/java/com/cqut/chen/Hello.java
+++ src/main/java/com/cqut/chen/Hello.java
@@ -8,5 +8,7 @@ public class Hello {
         System.out.println("主干添加");
         System.out.println("分支添加");
         System.out.println("在线更新");
+        System.out.println("克隆下来的项目");
+
     }
 }
